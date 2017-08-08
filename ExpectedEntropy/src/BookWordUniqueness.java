d0bb73eeefca8385cfe80ef9d6dc1110cfe100bc
kbaas <happyjack27@gmail.com>
2017-08-08 12:28:24 -0500

    k

---
+++ ExpectedEntropy/src/BookWordUniqueness.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import distributions.*;
+
+public class BookWordUniqueness {
+	/*
+minimize cross-entropy OF book given words
+OR maximize mi OF books AND words
+FOR a given word count
+	 */
+
+	double[][] book_word_count = null;
+	Vector<int[]> test_subsets = null;
+	
+	public void getBestSubsetOfSizeN(int N, double[][] book_word_count) {
+		Vector<int[]> test_subsets = new Vector<int[]>();
+	}
+
+}
