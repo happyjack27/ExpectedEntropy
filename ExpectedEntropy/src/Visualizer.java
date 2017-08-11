0fd4046c51bdf22accdb05a4e9b94aa04fd048ae
jimbrill <jimbrill@192.168.1.101>
2017-08-11 13:21:11 -0500

    vv

--- ExpectedEntropy/src/Visualizer.java
+++ ExpectedEntropy/src/Visualizer.java
@@ -26,5 +26,97 @@ public class Visualizer {
 	 * 
 	 * 
 	 */
+	
+	int num_vars = 4;
+	int num_dots = 100*100;
+	int last_dot = 0;
+	double totalH = 4;
+	double dots_per_H = 0;
+	double[] I;
+	double[] D;
+	int size = 0;
+	int[] dot_connections = new int[num_dots];
+	int[][] dot_coords = new int[num_dots][2];
+	
+	public void init() {
+		size = 0x01 << num_vars;
+		I = new double[size];
+		D = new double[size];
+		dots_per_H = (double)num_dots/totalH;
+		
+		//calculate dot_density
+		for( int bit_count = num_vars; bit_count > 0; bit_count--) {
+			for( int i = 0; i < size; i++) {
+				if( Integer.bitCount(i) == bit_count) {
+					D[i] = I[i] - totalParents(i);
+				}
+			}
+		}
+
+		//now attach dots
+		int i = 0;
+		double dot_remainder = 0;
+		double cur_remainder = D[i]*dots_per_H;
+		for( int d = 0; d < num_dots; d++) {
+			if( cur_remainder < 1) {
+				dot_remainder += cur_remainder;
+				if( dot_remainder >= 1) {
+					dot_connections[d] = i;
+					dot_remainder--;
+				} else {
+					d--;
+				}
+				i++;
+				cur_remainder = D[i]*dots_per_H;
+			} else {
+				dot_connections[d] = i;
+				cur_remainder--;
+			}
+		}
+	}
+	public double scoreMap(int[][] dot_coords) {
+		double[] counts = new double[num_vars];
+		double[][] centers = new double[num_vars][2];
+		double[] squared_distances = new double[num_vars];
+		
+		//compute centers
+		for( int d = 0; d < dot_connections.length; d++) {
+			for( int i = 0; i < num_vars; i++) {
+				if( ((0x01 << i) & dot_connections[d]) != 0 ) {
+					counts[i]++;
+					centers[i][0] += dot_coords[i][0];
+					centers[i][1] += dot_coords[i][1];
+				}
+			}
+		}
+		for( int i = 0; i < num_vars; i++) {
+			counts[i] = 1.0 / counts[i];
+			centers[i][0] *= counts[i];
+			centers[i][1] *= counts[i];
+		}
+		
+		//compute sum squared distance
+		double ssd = 0;
+		for( int d = 0; d < dot_connections.length; d++) {
+			for( int i = 0; i < num_vars; i++) {
+				if( ((0x01 << i) & dot_connections[d]) != 0 ) {
+					double dx = dot_coords[i][0] - centers[i][0];
+					double dy = dot_coords[i][1] - centers[i][1];
+					ssd += dx*dx + dy*dy;
+				}
+			}
+		}
+		return ssd;
+	}
+	
+	public double totalParents(int mask) {
+		double tot = 0;
+		for( int i = 0; i < size; i++) {
+			if( (i & mask) == mask && i != mask) {
+				tot += D[i];
+			}
+		}
+		return tot;
+	}
 
 }
